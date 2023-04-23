9ff04af0c765af062cbd690ebecb00ee29af05f7
Matias Molesini <Mhmolesini@gmail.com>
2023-04-23 17:55:28 -0300

    uso del useState

--- src/components/TodoSearch/index.jsx
+++ src/components/TodoSearch/index.jsx
@@ -3,16 +3,21 @@ import './TodoSearch.css'
 
 export function TodoSearch() {
 
+    const [searchValue, setSearchValue] = React.useState('')
+
     const OnSearchValueChange = (event) => {
         console.log(event.target.value)
+        setSearchValue(event.target.value)
     }
 
     return (
         <>
             <input 
                 className="input" 
-                placeholder="Search to Task"
+                placeholder="Search to Task" 
+                value={searchValue}
                 onChange={OnSearchValueChange}></input>
+            <p>{searchValue}</p>
         </>
     )
 }
