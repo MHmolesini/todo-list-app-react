01a7f275303eed5f060223ec6abc404d988333a4
Matias Molesini <Mhmolesini@gmail.com>
2023-04-23 18:33:02 -0300

    contador en el subtitulo

--- src/components/TodoCounter/index.jsx
+++ src/components/TodoCounter/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './TodoCounter.css';
 
-export function TodoCounter() {
+export function TodoCounter({total, completed}) {
+
     return (
         <>
             <h2 className='h2'>My Tasks</h2>
+            <h3>Has completado {completed} de {total} TODOs</h3>
         </>
     )
 }
